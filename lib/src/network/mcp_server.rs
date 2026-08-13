730b9a3536cebd0aa7ca2e58f77161254bec5f32
BytelyPlay <universalnebula2024@outlook.com>
2026-08-13 19:56:30 +0200

    Trying to figure out how to make a thread for this and compile it

--- lib/src/network/mcp_server.rs
+++ lib/src/network/mcp_server.rs
@@ -1,3 +1,34 @@
-struct McpServer {
-    
+use std::net::TcpListener;
+
+pub struct McpServer {
+    listener: TcpListener
+}
+
+impl McpServer {
+    pub fn listen(port: u16, ip: impl Into<String>) -> Result<Self, std::io::Error> {
+        let result = Self {
+                listener: TcpListener::bind(
+                    format!("{}:{}", ip.into(), port)
+                )?
+            };
+
+        result.begin_accepting();
+
+        Ok(
+            result
+        )
+    }
+    fn begin_accepting(&self) {
+        tokio::spawn(
+            async {
+                let mut buf = [0u8; 1024];
+
+                self.listener.accept();
+
+                loop {
+
+                }
+            }
+        );
+    }
 }
